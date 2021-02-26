c391991c12bb46952d66941815b7c0a5e41616cd
ejsmith13 <ejsmithwork@gmail.com>
2021-02-25 19:23:46 -0500

    schema and seeds files for db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Breakfast Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Hawaiian Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Cowboy Burger");
