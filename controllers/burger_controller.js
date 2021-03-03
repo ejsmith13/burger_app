6f73fd895fb4b504347519fe0463ec6dd5a9b589
ejsmith13 <ejsmithwork@gmail.com>
2021-03-02 21:52:38 -0500

    update to comments

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,24 +4,24 @@ const router = express.Router();
 
 const burger = require("../models/burger");
 
-// Create all our routes and set up logic within those routes where required.
+
 router.get("/", (req, res) => {
   burger.selectAll((data) => {
-    const hbsObject = {
+    const burgerObj = {
       burgers: data,
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    console.log(burgerObj);
+    res.render("index", burgerObj);
   });
 });
 
 router.get("/api/burgers", (req, res) => {
   burger.selectAll((data) => {
-    const hbsObject = {
+    const burgerObj = {
       burgers: data,
     };
-    console.log(hbsObject);
-    res.json(hbsObject);
+    console.log(burgerObj);
+    res.json(burgerObj);
   });
 });
 
@@ -30,7 +30,7 @@ router.get("/api/burgers", (req, res) => {
 router.post("/api/burgers", (req, res) => {
   console.log( `req: ${req}`);
   burger.insertOne(['burger_name'],[req.body.burger_name], (result) => {
-    // Send back the ID of the new quote
+    
     res.json({ id: result.insertId });
   });
 });
@@ -47,7 +47,7 @@ router.put("/api/burgers/:id", (req, res) => {
     selectionID,
     (result) => {
       if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
+        
         return res.status(404).end();
       }
       res.status(200).end();
--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,85 +1,77 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    if (event) {
-      console.info("DOM loaded");
-    }
-  
-    // UPDATE
-    const devourBtns = document.querySelectorAll(".devoured-btn");
-  
-    // Set up the event listener for the create button
-    if (devourBtns) {
-      devourBtns.forEach((button) => {
-        button.addEventListener("click", (e) => {
-          // Grabs the id of the element that goes by the name, "id"
-          const id = e.target.getAttribute("data-id");
-          const eaten = e.target.getAttribute("data-eaten");
-          console.log(id);
-          console.log(eaten);
-  
-          const changeEaten = {
-            devoured: eaten,
-          };
+  if (event) {
+    console.info("DOM loaded");
+  }
 
-          console.log(changeEaten)
-  
-          fetch(`/api/burgers/${id}`, {
-            method: "PUT",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-            },
-  
-            // make sure to serialize the JSON body
-            body: JSON.stringify(changeEaten),
-          }).then((response) => {
-            // Check that the response is all good
-            // Reload the page so the user can see the new quote
-            if (response.ok) {
-              console.log(`changed devourered to: ${changeEaten}`);
-              location.reload("/");
-            } else {
-              alert("something went wrong!");
-            }
-          });
-        });
-      });
-    }
-  
-    // CREATE
-    const newBurgerBtn = document.getElementById("create-form");
-  
-    if (newBurgerBtn) {
-      newBurgerBtn.addEventListener("submit", (e) => {
-        e.preventDefault();
-  
-        // Grabs the value of the textarea that goes by the name, "quote"
-        const newBurger = {
-          burger_name: document.getElementById("burger").value.trim(),
-          
+  const devourBtns = document.querySelectorAll(".devoured-btn");
+
+  // Set the event listener for the devour button
+  if (devourBtns) {
+    devourBtns.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        // Grabs the id of the element that goes by the name, "id"
+        const id = e.target.getAttribute("data-id");
+        const eaten = e.target.getAttribute("data-eaten");
+        console.log(id);
+        console.log(eaten);
+
+        const changeEaten = {
+          devoured: eaten,
         };
 
-        console.log(newBurger)
-  
-        // Send POST request to create a new quote
-        fetch("/api/burgers", {
-          method: "POST",
+        console.log(changeEaten);
+
+        fetch(`/api/burgers/${id}`, {
+          method: "PUT",
           headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
           },
-  
-          // make sure to serialize the JSON body
-          body: JSON.stringify(newBurger),
-        }).then(() => {
-          // Empty the form
-          document.getElementById("burger").value = "";
-  
-          // Reload the page so the user can see the new quote
-          console.log("Created a new burger!");
-          location.reload();
+
+          body: JSON.stringify(changeEaten),
+        }).then((response) => {
+          if (response.ok) {
+            console.log(`changed devourered to: ${changeEaten}`);
+            location.reload("/");
+          } else {
+            alert("something went wrong!");
+          }
         });
       });
-    }
-  
-   
-  });
+    });
+  }
+
+  // CREATE new burger
+  const newBurgerBtn = document.getElementById("create-form");
+
+  if (newBurgerBtn) {
+    newBurgerBtn.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      // Grabs the value of the textarea
+      const newBurger = {
+        burger_name: document.getElementById("burger").value.trim(),
+      };
+
+      console.log(newBurger);
+
+      fetch("/api/burgers", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+
+        // make sure to serialize the JSON body
+        body: JSON.stringify(newBurger),
+      }).then(() => {
+        // Empty the form
+        document.getElementById("burger").value = "";
+
+        // Reload the page
+        console.log("Created a new burger!");
+        location.reload();
+      });
+    });
+  }
+});
